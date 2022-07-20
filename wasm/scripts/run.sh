b23a000dd8de304641e2fd06f58a0ffbd12101a0
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-07-20 21:55:54 +0200

    Add wasm docker image

---
+++ wasm/scripts/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck >/dev/null && shellcheck "$0"
+
+# add debug info here
+cp -R "/template/.wasmd" /root
+mkdir -p /root/log
+wasmd start --rpc.laddr tcp://0.0.0.0:26657 --trace
